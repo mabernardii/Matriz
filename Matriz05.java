ca5e2aeb6f0bbce84528e8b785af5ddb0bc9ee66
mabernardii <manuela.bernardi1a@gmail.com>
2025-06-04 07:30:05 -0300

    Exercício 05 - Leia uma matriz 5x2 e crie uma terceira matriz támbem 5x2 com ovalor da soma dos elementos de mesmo índice.

---
+++ Matriz05.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+5.Desenvolva um programa que leia duas matrizes 5x2 e crie uma terceira matriz também 5x2
+com o valor da soma dos elementos de mesmo índice. Ex:
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Matriz05 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int[][] matriz1 = new int[5][2];
+        int[][] matriz2 = new int[5][2];
+        int[][] matrizSoma = new int[5][2];
+
+        System.out.println("Digite os elementos da primeira matriz (5x2):");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("Matriz1[" + i + "][" + j + "]: ");
+                matriz1[i][j] = ler.nextInt();
+            }
+        }
+
+        System.out.println("Digite os elementos da segunda matriz (5x2):");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("Matriz2[" + i + "][" + j + "]: ");
+                matriz2[i][j] = ler.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrizSoma[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+
+        System.out.println("Matriz resultante (Soma):");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matrizSoma[i][j] + "\t");
+            }
+            System.out.println();
+        }
+       
+     
+    }
+}
+
+
